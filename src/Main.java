751ff651bb2a70b8d8b35171b7f933fe2746d3ad
TomWulf <Tom.Wulf@uc.edu>
2023-02-20 16:51:40 -0500

    Completed ready for video

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main
         int age3 = 13;
         int age4 = 23;
 
+        double favCost1 = 23.50;
+        double favCost2 = 123.50;
+        double favCost3 = 423.50;
+        double favCost4 = 263.50;
+
         System.out.printf("Hello to %25s!\n", userName1);
         System.out.printf("Hello to %-25s!", userName1);
 
@@ -26,8 +31,12 @@ public class Main
         System.out.printf("%-25s (Age: %3d)\n", userName3, age3);
         System.out.printf("%-25s (Age: %3d)\n", userName4, age4);
 
+        System.out.println();
 
-
+        System.out.printf("%-25s (Age: %3d) Fav: %6.2f\n", userName1, age1, favCost1);
+        System.out.printf("%-25s (Age: %3d) Fav: %6.2f\n", userName2, age2, favCost2);
+        System.out.printf("%-25s (Age: %3d) Fav: %6.2f\n", userName3, age3, favCost3);
+        System.out.printf("%-25s (Age: %3d) Fav: %6.2f\n", userName4, age4, favCost4);
 
     }
 }
